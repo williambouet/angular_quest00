339b2a0cc5dbc1aa88181cfb88648d08a9e89bbe
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-02-14 15:03:45 +0100

    added component calculatrice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
+import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent
+    SquareComponent,
+    CalculatriceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculatrice/calculatrice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice',
+  templateUrl: './calculatrice.component.html',
+  styleUrls: ['./calculatrice.component.css']
+})
+export class CalculatriceComponent {
+
+}
