721e22c7e83cccef9b75da86e5c7fc8bf3aa2e61
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-02-14 14:53:48 +0100

    update app.component.css

--- src/app/square.ts
+++ src/app/square.ts
@@ -1,4 +1,5 @@
 export interface Square {
-    id: number;
-    name: string;
-  }
+  id: number;
+  name: string;
+  imageSrc: string;
+}
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Square } from '../square';
 
 @Component({
@@ -6,9 +6,22 @@ import { Square } from '../square';
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
-export class SquareComponent {
-    square: Square = {
+export class SquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  square: Square = {
     id: 1,
-    name: "ABC"
+    name: "ABC",
+    imageSrc: "https://placekitten.com/100/100"
   };
+
+  sayHello(): void {
+    alert("Coucou toi !");
+  }
+
 }
