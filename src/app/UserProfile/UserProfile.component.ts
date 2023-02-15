bf59d768a20c09d5867d09ec240bcb2a43507629
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-02-15 09:21:44 +0100

    added user class

--- src/app/UserProfile/UserProfile.component.ts
+++ src/app/UserProfile/UserProfile.component.ts
@@ -7,15 +7,17 @@ import { User } from '../user';
   styleUrls: ['./UserProfile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  user = {
-    firstname: "Will",
-    lastname: "iam",
-    age: 24,
-    quote: "Banane ça commence par un B mais normalement ça commence par un N...",
-    avatar: "https://img.passeportsante.net/1200x675/2021-05-03/i101971-banane-nu.webp"
-  }
-
-  constructor() { }
+  public user: User;
+  
+  constructor() {
+    this.user = new User(
+      "Will",
+      "iam",
+      24,
+      "Banane ça commence par un B mais normalement ça commence par un N...",
+      "https://img.passeportsante.net/1200x675/2021-05-03/i101971-banane-nu.webp"
+)
+   }
 
   ngOnInit() {
   }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,55 @@
-export interface User {
-  firstname: string;
-  lastname: string;
-  age: number;
-  quote?: string;
-  avatar?: string;
+export class User {
+    private _firstname: string;
+    private _lastname: string;
+    private _age: number;
+    public quote: string | undefined;
+    private _avatar: string | undefined;
+
+    constructor(
+        firstname: string,
+        lastname: string,
+        age: number,
+        quote: string = "",
+        avatar: string = ""
+    ) {
+    
+        this._firstname = firstname;
+        this._lastname = lastname;
+        this._age = age;
+        this.quote = quote;
+        this._avatar = avatar;
+    }
+    
+    public getFirstname(): string
+    {
+        return this._firstname;
+    }
+
+    public getLastname(): string
+    {
+        return this._lastname;
+    }
+
+    public getAge(): number
+    {
+        return this._age;
+    }
+
+    public getQuote(): string | undefined
+    {
+        return this.quote;
+    }
+
+    public setQuote(quote: string | undefined): void
+    {
+        this.quote = quote;
+    }
+
+    public getAvatar(): string | undefined
+    {
+        return this._avatar;
+    }
+
+
+
 }
