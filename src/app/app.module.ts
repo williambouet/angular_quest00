0a89785c26c562cdca5e0bba36e68147d9773d6c
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-02-15 10:26:05 +0100

    before challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    SquareComponent
-  ],
+    SquareComponent,
+      IngredientComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
