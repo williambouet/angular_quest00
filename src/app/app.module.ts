741f3861c2204631f4892e2bbde7fe05a91b9ef5
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-02-16 12:20:28 +0100

    refacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 
-import { Routes } from '@angular/router'; 
+import { Routes } from '@angular/router';
 import { ROUTES } from './app-routes';
 
 import { AppComponent } from './app.component';
@@ -21,9 +21,8 @@ import { ChangeMovieDirective } from './directive/changeMovie.directive';
 import { DisplayMovieDirective } from './directive/displayMovie.directive';
 import { NavbarComponent } from './navbar/navbar.component';
 
-
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
     SquareComponent,
     CalculatriceComponent,
@@ -33,14 +32,18 @@ import { NavbarComponent } from './navbar/navbar.component';
     DisplayGuestsDirective,
     MovieComponent,
     ChangeMovieDirective,
-    DisplayMovieDirective, 
+    DisplayMovieDirective,
     CardComponent,
-      NavbarComponent
-   ],
-  imports: [BrowserModule, AppRoutingModule,
-    FormsModule, FormsModule, RouterModule.forRoot(ROUTES)],
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-  
 export class AppModule {}
